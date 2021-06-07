22ed85c65c94255523067da6a8bf5fa7a867bbca
Kiran Kumar Mallavarapu <kmallavarapu@corp.miraclesoft.com>
2021-06-07 16:16:44 +0530

    added cross origin

--- src/main/java/com/db/register/RegisterController.java
+++ src/main/java/com/db/register/RegisterController.java
@@ -3,6 +3,7 @@ package com.db.register;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class RegisterController {
 
 	@Autowired
